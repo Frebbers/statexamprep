6612d8c7844ac6e4627f8af7b3e0876c3bbaa478
Frebbers <frederik.bode@hendrichsen.dk>
2024-12-09 20:46:26 +0100

    Added method 3.9

--- Solvers/Confidence Interval.py
+++ Solvers/Confidence Interval.py
@@ -12,3 +12,12 @@ print("95% confidence interval for mean estimate: " + str(mu_lower), str(mu_uppe
 s_lower = s * np.sqrt(n / stats.chi2.ppf(0.975, df=n - 1))
 s_upper = s * np.sqrt(n / stats.chi2.ppf(0.025, df=n - 1))
 print ("95% confidence interval for the standard deviation estimate: " + str(s_lower), str(s_upper))
+
+# confidence interval for mean estimate Method 3.9
+significance = 0.1
+n = 85
+mean = 20/n
+ci_lower = mean - stats.t.ppf(1-significance/2, df=n-1) * np.sqrt(mean*(1-mean)/n)
+ci_upper = mean + stats.t.ppf(1-significance/2, df=n-1) * np.sqrt(mean*(1-mean)/n)
+print("90% confidence interval for estimate: " + str(ci_lower), str(ci_upper))
+
