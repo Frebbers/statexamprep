aaefad688ea9707ecf1321feabc2e64ab6948a9c
Frebbers <frederik.bode@hendrichsen.dk>
2024-12-14 12:31:33 +0100

    added binomial distribution

--- Solvers/Distributions.py
+++ Solvers/Distributions.py
@@ -3,8 +3,9 @@
 import math
 
 import numpy as np
+from scipy.stats import binom
 
-lam = 2.5
+lam = 0.45 # the average number of events in a given interval
 x = 5 # the number of events we are interested in seeing
 
 #The probability of seeing exactly x events in a given interval
@@ -18,3 +19,16 @@ print("The probability of seeing less than x events in a given interval " + str(
 #the probability of seeing x or more events in a given interval
 prob2 = 1-prob
 print("The probability of seeing x or more events in a given interval " + str(prob2))
+
+## Binomial distribution
+n = 190  # the number of trials
+p = 0.45  # the probability of success
+k = 100  # the number of successes we are interested in seeing
+
+#The probability of seeing exactly x successes in n trials
+prob3 = binom.pmf(k, n, p)
+print("The probability of seeing exactly x successes in n trials " + str(prob3))
+
+#The probability of seeing more than x successes in n trials
+prob4 = 1 - binom.cdf(k - 1, n, p)
+print("The probability of seeing more than x successes in n trials " + str(prob4))
