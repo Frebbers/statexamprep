80101024fa9f95cdf6696732364fbf256f37261e
Frebbers <frederik.bode@hendrichsen.dk>
2024-12-14 14:06:09 +0100

    merged stuff

--- Solvers/Distributions.py
+++ Solvers/Distributions.py
@@ -1,34 +1,36 @@
-#Discrete distributions
-## Poisson distribution
+# Discrete distributions
+# Poisson distribution
 import math
 
 import numpy as np
 from scipy.stats import binom
 
-lam = 2.5 # the average number of events in a given interval
-x = 10 # the number of events we are interested in seeing
+lam = 0.45 # the average number of events in a given interval
+x = 5 # the number of events we are interested in seeing
 
-#The probability of seeing exactly x events in a given interval
+# The probability of seeing exactly x events in a given interval
 prob = (np.exp(-lam) * lam**x) / math.factorial(x)
 print("The probability of seeing exactly x events in a given interval " + str(prob))
 
-#The probability of seeing less than x events in a given interval
+# The probability of seeing less than x events in a given interval
 prob = sum([(np.exp(-lam) * lam**i) / math.factorial(i) for i in range(x)])
 print("The probability of seeing less than x events in a given interval " + str(prob))
 
-#the probability of seeing x or more events in a given interval
+# The probability of seeing x or more events in a given interval
 prob2 = 1-prob
 print("The probability of seeing x or more events in a given interval " + str(prob2))
 
-## Binomial distribution
+
+# Binomial distribution
 n = 190  # the number of trials
 p = 0.45  # the probability of success
 k = 100  # the number of successes we are interested in seeing
 
-#The probability of seeing exactly x successes in n trials
+# The probability of seeing exactly k successes in n trials
 prob3 = binom.pmf(k, n, p)
 print("The probability of seeing exactly x successes in n trials " + str(prob3))
 
-#The probability of seeing more than x successes in n trials
+# The probability of seeing more than k successes in n trials
 prob4 = 1 - binom.cdf(k - 1, n, p)
 print("The probability of seeing more than x successes in n trials " + str(prob4))
+
